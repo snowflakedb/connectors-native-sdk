609be5f42103872c463e10fcd041f1d2895f362c
Piotr Hachaj <piotr.hachaj@snowflake.com>
2023-09-29 11:30:30 +0200

    Added example push based java connector. (#3)

---
+++ examples/example-push-based-java-connector/example-push-based-java-connector-agent/src/main/java/com/snowflake/connectors/sdk/example_push_based_java_connector/DataSource.java
@@ -0,0 +1,11 @@
+package com.snowflake.connectors.sdk.example_push_based_java_connector;
+
+import java.util.List;
+import java.util.Map;
+
+public interface DataSource {
+
+  List<Map<String, Object>> fetchInitialRecords(int recordCount);
+
+  List<Map<String, Object>> fetchRecords(int recordCount);
+}
