a2f3a54194816ac78754ee5c7a985e21f3c8552d
Rafal Zukowski <rafal.zukowski@snowflake.com>
2023-12-14 16:17:01 +0100

    NO-CONN: Remove unused java files

--- examples/example-github-java-connector/java-module/src/main/java/com/snowflake/connectors/sdk/example_github_connector/application/Infrastructure.java
+++
@@ -1,7 +0,0 @@
-package com.snowflake.connectors.sdk.example_github_connector.application;
-
-public class Infrastructure {
-    public static final String SCHEMA = "PUBLIC";
-    public static final String APP_ROLE = "APP_ROLE";
-    public static final String TASKS_SCHEMA = "TASKS";
-}
